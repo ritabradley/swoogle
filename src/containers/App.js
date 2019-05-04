3a59c6885a7eff08fd145204389a3aaf343c8233
Rita Bradley <contact@ritabradley.com>
2019-05-04 07:19:34 -0700

    update readme

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Swoogle</h1>
+      <p>May The 4th Be With You</p>
+    </div>
+  );
+};
+
+export default App;
